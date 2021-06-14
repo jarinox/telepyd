f4103c01c54934b7d9e3461319773a4f6fa072bf
jarinox <45308098+jarinox@users.noreply.github.com>
2021-06-14 18:20:27 +0200

    upgrade to python-droit v1.1.1

--- droit-plugins-telegram/output/eval.inline/main.py
+++ droit-plugins-telegram/output/eval.inline/main.py
@@ -1,8 +1,8 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-def keyboard(data, db):
+def keyboard(data, variables, db):
     kb = [[]]
     for i in range(1, len(data)):
         kb[0].append(InlineKeyboardButton(data[i], callback_data=data[0]+str(i)))
     db.temp["inline"] = InlineKeyboardMarkup(kb)
-    return "", db
+    return "", variables, db
